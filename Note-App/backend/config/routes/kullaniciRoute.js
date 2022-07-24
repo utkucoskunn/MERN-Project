005161df6177e0e9bc80d5590794af19fb0dc370
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-07-24 14:28:02 +0300

    Add files via upload

---
+++ Note-App/backend/config/routes/kullaniciRoute.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const {registerKullanici, loginKullanici, getKullanici}=require('../controllers/kullaniciController');
+const {kullaniciKontrol}=require('../middlewares/authMiddleware');
+const router=express.Router();
+
+
+router.post('/',registerKullanici);
+router.post('/login',loginKullanici);
+router.get('/kullanici',kullaniciKontrol,getKullanici);
+module.exports=router;
